f09ce482d796ddb50061f2fea837056d55ac3d58
Tanmay Sharma <sharma.tanmay4002@gmail.com>
2025-07-24 11:48:01 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,12 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
+// What it does: Imports Box, a Chakra UI component that works like a <div> but with Chakra styling.
+// useColorModeValue(light, dark) is a Chakra hook that returns a value based on the current color mode (light or dark).
+// Example:
+// bg={useColorModeValue("gray.100", "gray.900")}
+// Means:
+// If light mode → use gray.100
+// If dark mode → use gray.900
+
 import { Route, Routes } from "react-router-dom";
 
 import CreatePage from "./pages/CreatePage";
